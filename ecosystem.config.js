8b1f262d8e077a896550c8961380f3ac71ef5178
Dilshan Hesara <hesarsdilshan@gmail.com>
2026-08-27 01:50:21 +0530

    add ps2 ecosystem for cloud-sql-auth-proxy up

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  apps : [{
+  apps : [
+      {
+      name: "cloud-sql-auth-proxy",
+      script: "./cloud-sql-proxy auton-dh:asia-southeast1:mysql-vm auton-dh:asia-southeast1:postgres-vm --private-ip",
+      log_file: "./logs/cloud-sql-auth-proxy.log"
+      },
+
+      {
     name   : "service-inventory",
     script : "java -jar ./D-pay-Inventory-Service/target/Inventory-Service-1.0.0.jar",
     log_file:"./logs/service-inventory.log",
